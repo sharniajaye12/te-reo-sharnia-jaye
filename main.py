16087e1f2cf9a3e998856a966bec99f7cfd68098
Sharnia Cochrane <105686312+sharniajaye12@users.noreply.github.com>
2022-05-26 22:05:38 +0000

    i put things on the black screen

---
+++ main.py
@@ -0,0 +1,10 @@
+print("what is your name")
+n = input ()
+print("kia ora " + n )
+print("what is shut up in maaori")
+answer = input ()
+answer = answer .lower()
+if answer == "kopani":
+  print ("you are correct")
+else:
+  print("you are wrong")
